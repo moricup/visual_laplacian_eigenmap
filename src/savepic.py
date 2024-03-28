a50b1999498353200306b1251df952a10d097b9b
moricup <morisho.0123@gmail.com>
2024-03-28 20:32:34 +0900

    Co-authored-by: ekatsym <ekatsym@users.noreply.github.com> Co-authored-by: haskey1236 <haskey1236@users.noreply.github.com>

--- src/savepic.py
+++ src/savepic.py
@@ -1,14 +1,15 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
 def savepic3D(x, y, z, picname, elev, azim):
     fig = plt.figure(figsize=(9, 9), facecolor="w")
     ax = fig.add_subplot(111, projection="3d")
-    ax.scatter(x, y, z)
+    ax.scatter(x, y, z, c = np.linspace(start=0.0, stop = 1.0, num = len(x)), cmap="hsv")
     ax.view_init(elev = elev, azim = azim)
     plt.savefig(picname)
 
 def savepic2D(x, y, picname):
     fig = plt.figure(figsize=(9, 9), facecolor="w")
     ax = fig.add_subplot(111)
-    ax.scatter(x, y)
+    ax.scatter(x, y, c = np.linspace(start=0.0, stop = 1.0, num = len(x)), cmap="hsv")
     plt.savefig(picname)
